d4e07a97dd777f9c71745def4bf553e50f6a1f6b
Ernesto Alvarado <alvaradoernesto@hotmail.com>
2020-06-25 20:53:11 -0300

    intento 2

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,5 +2,4 @@ class Cocktail < ApplicationRecord
   validates :name, uniqueness: true, presence: true
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
-  has_one_attached :photo
 end
